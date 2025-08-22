0793fde8019c310059e674d17015e3c6ccb454dc
JoshBTech-eng <jbyas9116@floridapoly.edu>
2025-08-22 14:38:46 +0000

    Created the 1st Program

---
+++ 01_intro_to_coding/First_Program.c
@@ -0,0 +1,17 @@
+ /*
+ 
+ Multi-line comment 
+ 08/20/2025
+ Joshua Byas
+ The First Program in C.
+ 
+ */
+
+ // One line comment 
+
+#include <stdio.h>
+
+// Create the main function
+int main (void)  {
+    printf("Hello World\n");
+}
